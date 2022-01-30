1c1176d22815691c4af4a213a00ac1c20b855e41
Noctis Kirishiro Yamazaki-Triendl <45600261+nocdoggo@users.noreply.github.com>
2022-01-30 00:26:33 -0500

    Create index.js
    The kickstart with the file-system module loading

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,4 @@
+const fs = require('fs');       // Use a variable to call out the file-system module
+
+const hello = 'Hello world';    // Use const to initiate a new variable
+console.log(hello);             // Then, use the console log function to print the result out in the terminal
